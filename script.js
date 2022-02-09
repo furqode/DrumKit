8e9d73bb5d6ed2853f4c11b3c1e939a974184041
Fury <furqode@gmail.com>
2022-02-09 17:21:39 +0500

    add function to remove transition after the audio is done playing and make the code look a little better

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-window.addEventListener("keydown", function(e) {
-    const audio =  document.querySelector(`audio[data-key="${e.keyCode}"]`)
-    if(!audio) return; //stop the function if no audio is available
-    audio.currentTime=0; // resets the time on audio
-    audio.play();
+function removeTransition(e) {
+    if (e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+  }
 
-})
+  function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    key.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+  window.addEventListener('keydown', playSound);
